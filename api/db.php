3ce4e05cafc1ea016146d7069695a40098f9eb08
Deepak J <deepaknaidu1212@gmail.com>
2025-10-01 10:12:11 +0530

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,3 +1,6 @@
+ini_set('upload_max_filesize', '20M');
+ini_set('post_max_size', '20M');
+
 <?php
 $host = "deepakmysqldb.mysql.database.azure.com";   // your database host
 $user = "mysqladmin@deepakmysqldb";        // database username
@@ -10,3 +13,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
